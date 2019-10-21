2a79725e82f359b334883ba3fd0a1f72201ec2d8
Steven P. Kennedy <umbrylax@gmail.com>
2019-10-21 16:13:07 -0600

    can get keeps in every way besides vaults

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -8,129 +8,129 @@ using ShoeStore.Services;
 
 namespace ShoeStore.Controllers
 {
-    [Authorize]
-    [ApiController]
-    [Route("/api/[controller]")]
-    public class OrdersController : ControllerBase
+  [Authorize]
+  [ApiController]
+  [Route("/api/[controller]")]
+  public class OrdersController : ControllerBase
+  {
+
+    private readonly OrdersService _ss;
+    public OrdersController(OrdersService ss)
     {
-
-        private readonly OrdersService _ss;
-        public OrdersController(OrdersService ss)
-        {
-            _ss = ss;
-        }
-
-        [HttpGet]
-        public ActionResult<IEnumerable<Order>> Get()
-        {
-            try
-            {
-                return Ok(_ss.Get());
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpGet("{id}")]
-        public ActionResult<Order> Get(int id)
-        {
-            try
-            {
-                return Ok(_ss.Get(id));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-
-        [HttpGet("{id}/shoes")]
-        public ActionResult<Order> GetShoes(int id)
-        {
-            try
-            {
-                return Ok(_ss.GetShoes(id));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpPost]
-        public ActionResult<Order> Create([FromBody] Order newOrder)
-        {
-            try
-            {
-                newOrder.UserId = HttpContext.User.FindFirstValue("Id");
-                return Ok(_ss.Create(newOrder));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-
-        [HttpPut("{id}")]
-        public ActionResult<Order> Edit([FromBody] Order newOrder, int id)
-        {
-            try
-            {
-                newOrder.Id = id;
-                return Ok(_ss.Edit(newOrder));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpPut("{id}/addShoe")]
-        public ActionResult<string> AddShoeToOrder([FromBody] Shoe shoe, int id)
-        {
-            try
-            {
-                string userId = HttpContext.User.FindFirstValue("Id");
-                return Ok(_ss.AddShoe(id, shoe.Id, userId));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpPut("{id}/removeShoe")]
-        public ActionResult<string> RemoveShoeFromOrder([FromBody] ShoeOrder so, int id)
-        {
-            try
-            {
-                so.OrderId = id;
-                string userId = HttpContext.User.FindFirstValue("Id");
-                return Ok(_ss.RemoveShoe(so, userId));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpDelete("{id}")]
-        public ActionResult<string> Delete(int id)
-        {
-            try
-            {
-                var userId = HttpContext.User.FindFirstValue("Id");
-                return Ok(_ss.Delete(id, userId));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
+      _ss = ss;
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Order>> Get()
+    {
+      try
+      {
+        return Ok(_ss.Get());
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Order> Get(int id)
+    {
+      try
+      {
+        return Ok(_ss.Get(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+
+    [HttpGet("{id}/shoes")]
+    public ActionResult<Order> GetShoes(int id)
+    {
+      try
+      {
+        return Ok(_ss.GetShoes(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<Order> Create([FromBody] Order newOrder)
+    {
+      try
+      {
+        newOrder.UserId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_ss.Create(newOrder));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+
+    [HttpPut("{id}")]
+    public ActionResult<Order> Edit([FromBody] Order newOrder, int id)
+    {
+      try
+      {
+        newOrder.Id = id;
+        return Ok(_ss.Edit(newOrder));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPut("{id}/addShoe")]
+    public ActionResult<string> AddShoeToOrder([FromBody] Shoe shoe, int id)
+    {
+      try
+      {
+        string userId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_ss.AddShoe(id, shoe.Id, userId));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPut("{id}/removeShoe")]
+    public ActionResult<string> RemoveShoeFromOrder([FromBody] ShoeOrder so, int id)
+    {
+      try
+      {
+        so.OrderId = id;
+        string userId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_ss.RemoveShoe(so, userId));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<string> Delete(int id)
+    {
+      try
+      {
+        var userId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_ss.Delete(id, userId));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+  }
 }
